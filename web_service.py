7889d3c421d588e973e8a843ea559296788f4296
majerzz <juliamjrzzz@gmail.com>
2022-12-16 04:28:11 +0600

    tried adding html (some pages work)

--- web_service.py
+++ web_service.py
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('one.html')
 
+@app.route("/2")
+def index2():
+    return render_template('two.html')
+
+@app.route('/3', methods=['GET', 'POST'])
+def index3():
+    return render_template('three.html')
+
+@app.route('/4', methods=['GET', 'POST'])
+def index4():
+    return render_template('four.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
